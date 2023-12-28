e71a4a59d5e9eefe761717ec238d234f4a651731
Patryk Wisniewski <patrykw@outlook.fr>
2023-12-28 18:55:32 +0100

    General improvements and fix #3
    Merge control variables with "table de passage". Fix issues with arrondissements in Paris, Lyon and Marseille. Improve inflation calculations by linear interpolation per quarter.

--- src/controlvariables.py
+++ src/controlvariables.py
@@ -7,6 +7,9 @@ path = "C:/Users/patry/Documents/GitHub/interestnpy"
 
 #IMPORT UNEMPLOYMENT DATA
 #source https://www.insee.fr/fr/statistiques/1893230
+t_passage = pd.read_feather(path+"/data/interim/tble_de_passage_py.feather")[["COM","ZE","LIB_COM", "LIB_ZE", "EPCI", "LIB_EPCI"]].drop_duplicates()
+
+
 unemployement = pd.read_excel(path+"/data/external/unemployment/chomage-zone-t1-2003-t3-2023.xlsx", 
                                sheet_name="txcho_ze", skiprows=5)
 
@@ -35,38 +38,37 @@ unemployement_long = unemployement_long[pd.to_numeric(unemployement_long['Date']
 
 income2014 = pd.read_excel(path+"/data/external/revenus/indic-struct-distrib-revenu-2014-COMMUNES/indic-struct-distrib-revenu-2014-COMMUNES/FILO_DISP_COM.xls", 
                                sheet_name="ENSEMBLE", skiprows=5)
+income2014 = income2014[["CODGEO","Q214"]]
 
 income2019 = pd.read_excel(path+"/data/external/revenus/indic-struct-distrib-revenu-2019-COMMUNES/FILO2019_DISP_COM.xlsx", 
                                sheet_name="ENSEMBLE", skiprows=5)
+income2019 = income2019[["CODGEO","Q219", "GI19"]]
 
 #MERGE 
-income2014_2019 = pd.merge(income2014, income2019, how='inner', on = ["CODGEO","LIBGEO"])
-
-#Keep CODGEO, LIBGEO , quartiles and Gini 
-income2014_2019 = income2014_2019[["CODGEO","LIBGEO","Q114","Q214","Q314","GI14","Q119","Q219","Q319","GI19"]]
+income2014_2019 = pd.merge(income2014, income2019, how='inner', on = ["CODGEO"])
 
 #ADD column Mediane_evol_diff to observe the evolution of available income between 2019 and 2019
 income2014_2019["med_change"] = income2014_2019["Q219"]/income2014_2019["Q214"]
 income2014_2019["CODGEO"] = income2014_2019["CODGEO"].astype(str) #preventive bug correction
 
 #IMPORT POPULATION AND SURFACE
-#source https://www.insee.fr/fr/statistiques/3698339
-#source https://www.insee.fr/fr/statistiques/2521169
-population = pd.read_excel(path+"/data/external/pop_density/base-pop-historiques-1876-2020.xlsx", 
-                               sheet_name="pop_1876_2020", skiprows=5)
-population = population[["CODGEO","PMUN14","PMUN19"]]
+#https://www.insee.fr/fr/statistiques/7632565
+population = pd.read_excel(path+"/data/external/pop_density/base-cc-serie-historique-2020.xlsx", 
+                               sheet_name="COM_2020", skiprows=5)[["CODGEO","P20_POP","P14_POP", "SUPERF"]]
+population = population[~population['CODGEO'].isin(["75056", "69123", "13055"])]  #Remove Paris, Lyon, Marseille                             
+                               
+population_arr = pd.read_excel(path+"/data/external/pop_density/base-cc-serie-historique-2020.xlsx", 
+                               sheet_name="ARM_2020", skiprows=5)[["CODGEO","P20_POP","P14_POP", "SUPERF"]]
 
+population_arr.loc[population_arr['CODGEO'] == "75112", 'SUPERF'] = 6.37 #correct for Bois de Vincennes
+population_arr.loc[population_arr['CODGEO'] == "75116", 'SUPERF'] = 7.91 #correct for Bois de Boulogne
 
-surface = pd.read_excel(path+"/data/external/pop_density/base_cc_comparateur.xlsx",sheet_name="COM",skiprows=5) #IF NOT RUNNING SAVE THE EXCEL FILE AND IT WILL WORK
-surface = surface[["CODGEO","SUPERF"]]
-surface["CODGEO"] = surface["CODGEO"].astype(str) #bug correction
-#MERGE
-densite = pd.merge(population,surface,how="inner",on=["CODGEO"])
+densite = pd.concat([population, population_arr], axis=0, ignore_index=True)
+
 
 #create popdensity2014 and popdensity2019 : population/surface
-densite["popdensity2014"] = densite["PMUN14"] / densite["SUPERF"]
-densite["popdensity2019"] = densite["PMUN19"] / densite["SUPERF"]
-densite = densite[["CODGEO","popdensity2014","popdensity2019"]]
+densite["popdensity2014"] = densite["P14_POP"] / densite["SUPERF"]
+densite["popdensity2019"] = densite["P20_POP"] / densite["SUPERF"]
 densite["CODGEO"] = densite["CODGEO"].astype(str) #preventive bug correction
 
 
@@ -89,8 +91,9 @@ control_var = pd.merge(control_var,physicist,how="inner",on="CODGEO")
 criminality = pd.read_csv(path+"/data/external/criminality/donnee-data.gouv-2022-geographie2023-produit-le2023-07-17.csv",sep=",")
 criminality = criminality.rename(columns = {"CODGEO_2023":"CODGEO"})
 criminality['tauxpourmille'] = criminality['tauxpourmille'].fillna(criminality['complementinfotaux'])
-#For year 2019
 criminality19 = criminality[criminality["annee"].isin([19])]
+criminality19['tauxpourmille'] = criminality19['tauxpourmille'].str.replace(",", ".").astype(float)
+
 
 burglary19 = criminality19[criminality19["classe"].isin(["Cambriolages de logement"])] #around 70% of Nan in the column tauxpourmille
 burglary19 = burglary19[["CODGEO","tauxpourmille"]]
@@ -114,6 +117,9 @@ control_var = pd.merge(control_var,burglary19,how="left",on="CODGEO")
 control_var = pd.merge(control_var,assault19,how="left",on="CODGEO")
 control_var = pd.merge(control_var,otherassault19,how="left",on="CODGEO")
 control_var = pd.merge(control_var,destruction19,how="left",on="CODGEO")
+
+#MERGE WITH TABLE DE PASSAGE
+control_var = pd.merge(t_passage,control_var,how="right",right_on="CODGEO", left_on="COM")
 
 # Writing output
 feather.write_feather(control_var, path+"/data/interim/TI_controls.feather")
