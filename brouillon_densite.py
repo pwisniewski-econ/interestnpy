15a59ee96d1d37df2a124c924cb923d9f0880953
RemiCSK <remi.calvet1@gmail.com>
2023-12-30 21:35:28 +0100

    add physicist map plot

--- brouillon_densite.py
+++ brouillon_densite.py
@@ -44,13 +44,36 @@ plt.show()
 P14_POP_by_ZE = control_var.groupby('ZE')['P14_POP'].sum() #population niveau ZE en 2014
 dfQ214 = control_var
 dfQ214["Q214_pondere"] = control_var['Q214'] * control_var["P14_POP"] #multiplie revenu médian par la population
-dfQ214 = dfQ214.groupby('ZE')['Q214_pondere'].sum() 
-dfQ214 = pd.merge(dfQ214,P14_POP_by_ZE,on="ZE")
-dfQ214["Q214_ZE"] = dfQ214["Q214_pondere"]/dfQ214["P14_POP"]
+dfQ214 = dfQ214.groupby('ZE')['Q214_pondere'].sum() #agrege par ZE en sommant
+dfQ214 = pd.merge(dfQ214,P14_POP_by_ZE,on="ZE") 
+dfQ214["Q214_ZE"] = dfQ214["Q214_pondere"]/dfQ214["P14_POP"] #divise le revenu médian pondéré par la population de la ZE pour avoir le revenu médian de la ZE
 dfQ214 = dfQ214[["Q214_ZE"]]
  
 full_map2 = ze_shp.merge(dfQ214, on="ZE", how="left")
 
 plot_map(full_map2, 
          'Q214_ZE', "Revenu médian en 2014 par zone d'emploi", 
+         "YlGnBu")
+
+
+
+
+
+#Création d'un dataframe avec l'accès aux soins par zone d'emploi pour faire une carte
+P14_POP_by_ZE = control_var.groupby('ZE')['P14_POP'].sum() #population niveau ZE en 2014
+dfQ214 = control_var
+dfQ214["Physicist_access_pondere"] = control_var['Physicist_access'] * control_var["P14_POP"] #multiplie Physicist_access par la population
+dfQ214 = dfQ214.groupby('ZE')['Physicist_access_pondere'].sum() 
+dfQ214 = pd.merge(dfQ214,P14_POP_by_ZE,on="ZE")
+dfQ214["Physicist_access_ZE"] = dfQ214["Physicist_access_pondere"]/dfQ214["P14_POP"]
+dfQ214 = dfQ214[["Physicist_access_ZE"]]
+dfQ214["Physicist_access_ZE"] = dfQ214["Physicist_access_ZE"].astype(float)
+ 
+full_map3 = ze_shp.merge(dfQ214, on="ZE", how="left")
+
+
+
+
+plot_map(full_map3, 
+         'Physicist_access_ZE', "Physicist_access en 2014 par zone d'emploi", 
          "YlGnBu")
